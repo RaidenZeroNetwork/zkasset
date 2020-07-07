3e1445ea5d9050298b478b659588427d70c21ceb
lychees <lychees67@gmail.com>
2020-07-07 00:28:11 -0700

    build: deploy contract

--- PROTOCOL/packages/protocol/truffle-config.js
+++ PROTOCOL/packages/protocol/truffle-config.js
@@ -164,7 +164,7 @@ module.exports = {
         },
         goerli: {
             provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`), //goerliProvider,
-            gas: 1e6,
+            gas: 7e6,
             gasPrice: toHex(toWei('1', 'gwei')),
             network_id: '5',
             confirmations: 3,
